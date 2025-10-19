a88f345daf7381b359db70811b7f956f814caa1a
thefauxpastrouper <adityakumar997399@gmail.com>
2025-10-19 13:19:43 +0530

    Added Helius rpc url

--- src/utils/getProvider.ts
+++ src/utils/getProvider.ts
@@ -2,7 +2,7 @@ import { AnchorProvider, setProvider } from "@coral-xyz/anchor";
 import { Connection } from "@solana/web3.js";
 
 export const getProvider = () => {
-  const connection = new Connection("https://api.devnet.solana.com");
+  const connection = new Connection("https://devnet.helius-rpc.com/?api-key=914ba64f-6706-4ba0-b320-bd35b6371e15");
   const provider = new AnchorProvider(connection, window.solana, {});
   setProvider(provider);
   return provider;
